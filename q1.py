0e0492a3cdac6269ce693f999b915c7cc8a5f112
Tom Rutter <67977402+TomRutter42@users.noreply.github.com>
2024-01-24 05:07:09 -0800

    Update q1.py

--- q1.py
+++ q1.py
@@ -6,12 +6,15 @@
 
 import numpy as np
 import pandas as pd
+from scipy.optimize import minimize
 import statsmodels.formula.api as smf
 import numpy as np
 
 ## 
 from warnings import simplefilter 
 simplefilter(action="ignore", category=pd.errors.PerformanceWarning) # F
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 # set seed 
 np.random.seed(123)
@@ -81,7 +84,7 @@ data = data.copy()
 
 ## New dataset to store the residuals, with three variables: year, id68, resid
 
-residuals_data = pd.DataFrame(columns = ["year", "id68", "resid"])
+residuals_data = pd.DataFrame(columns = ["year", "id68", "hage", "hpersno", "resid"])
 
 for year in data.year.unique():
     ## subset the data to the year
@@ -92,7 +95,7 @@ for year in data.year.unique():
     # print(reg.summary())
     ## Store the residuals in residuals_data
     ## concat axis = 0 means you are pasting together the columns, i.e., you get a row reading across for each entry
-    data_to_append = pd.concat([data_year[["year", "id68"]], reg.resid], axis = 1)
+    data_to_append = pd.concat([data_year[["year", "id68", "hage", "hpersno"]], reg.resid], axis = 1)
 
     ## axis = 1 means you are pasting together the columns, i.e., you are stacking entries on top of each other. 
     residuals_data = pd.concat([residuals_data, data_to_append], axis = 0)
@@ -271,7 +274,126 @@ for tp in time_pairs:
 print(cov_mat_sim - cov_mat)
 print(cov_mat)
 print(cov_mat_sim)
+# looks good 
 
-# print(sim_data.head(100))
-# print(sim_data[sim_data.time == 0].head(100))
-# print(sim_data[sim_data.time == 1].head(100))
+
+# ==============================================================================
+
+# GMM
+
+## Define Helper Functions
+
+### Take your parameter vector, the empirical covariance matrix (to size the theoretical matrix), and the time pairs
+
+def calculate_m_theoretical(theta, m_empirical, age_combinations):
+    # Create m theoretical
+    m_theoretical = np.empty_like(m_empirical)
+    m_theoretical[:] = np.nan
+
+    # Population Theoretical matrix
+    for x in age_combinations:
+        j = np.min(x)
+        h = np.max(x) - j # distance between ages
+
+        # Calculate E(e_ij ^2)
+        if j > 0:
+            var_eps_ij = (theta[0]**(2*j)) * 0 + np.sum([theta[0]**(2*(j - k)) * theta[2] for k in np.arange(1, j+1) ])
+        else:
+            var_eps_ij = 0
+
+        # Covariance
+        if x[0] != x[1]:    
+            covar = theta[3] + (theta[0]**h) *var_eps_ij
+            m_theoretical[x[0], x[1]] =  m_theoretical[x[1], x[0]] = covar
+        # Now the variance
+        else:
+            m_theoretical[j, j] = theta[3] + theta[1]  + var_eps_ij
+
+    return m_theoretical
+
+# Take your parameter vector, the empirical covariance matrix, the time pairs
+# returns the GMM objective, which is the sum of squared differences between the empirical and theoretical covariance matrices
+# and we'll try to minimize
+
+def gmm(theta, m_empirical, ages):
+
+    # All combinations + doubles
+    age_combinations = list(itertools.combinations(ages, 2))  + [(a,a) for a in ages]
+
+    # Calculate theoretical moments
+    m_theoretical = calculate_m_theoretical(theta, m_empirical, age_combinations)
+
+    # Extract moments for GMM
+    m_theoretical_vec = np.column_stack(m_theoretical[np.tril_indices(m_theoretical.shape[0])]).transpose()
+    m_empirical_vec =  np.column_stack(m_empirical[np.tril_indices(m_empirical.shape[0])]).transpose()
+
+    # Make weighting matrix:
+
+    if type(weight) == str  and weight == "identity":
+        weight = np.identity(len(m_theoretical_vec))
+    elif type(weight) is np.ndarray:
+        weight = weight # matrix given
+    else: 
+        weight = np.identity(len(m_theoretical_vec))
+
+    # Take difference between moments
+    mt_me = m_theoretical_vec - m_empirical_vec
+    objective = mt_me.transpose() @ mt_me
+
+    return objective
+
+# # Remove NA
+# residuals_data = resid_income_df.query("resid.notnull()", engine = 'python')
+# Add effective age
+residuals_data["effective_age"] = residuals_data["hage"] - 21
+residuals_data["effective_age_norm"]= residuals_data["effective_age"] - np.min(residuals_data["effective_age"]) # normalize min age to 0
+
+ages = np.sort(residuals_data.effective_age_norm.unique().astype(int))
+
+ages_min = np.min(ages)
+ages_max = np.max(ages)
+
+# Initialize
+rho = 0.9
+sigma_sq_alpha = 0.05
+sigma_sq_eta = 0.2
+sigma_sq_ups = 0.1
+theta0 = np.array([rho, sigma_sq_ups, sigma_sq_eta, sigma_sq_alpha])
+
+# Empty covariance matrix
+m_empirical = np.empty( [ages_max - ages_min + 1, ages_max - ages_min + 1] )
+m_empirical[:] = np.nan
+
+
+# All combinations + doubles
+age_combinations = list(itertools.combinations(ages, 2))  + [(a,a) for a in ages]
+
+for x in age_combinations:
+    # Covariance
+    if x[0] != x[1]:             
+        temp = residuals_data.query("effective_age_norm == @x[0] | effective_age_norm == @x[1]")
+
+        # Covariance calculation
+        temp_covar = temp.groupby(["id68", "hpersno" ]).filter(lambda x: x["hage"].count() == 2).groupby(["id68", "hpersno" ]).agg( cov = ("resid",np.prod) ).mean()
+        # Populate matrix (both entries)
+        m_empirical[x[0], x[1]] = temp_covar
+        m_empirical[x[1], x[0]] = temp_covar
+    else:
+        temp = residuals_data.query("effective_age_norm == @x[0]")
+        temp_var = temp.groupby(["id68", "hpersno" ]).agg( var = ("resid",  lambda x: x**2 ) ).mean()
+        m_empirical[x[0], x[0]] = temp_var
+
+print(m_empirical)
+
+# Run GMM
+gmm_estimate = minimize(gmm, theta0,  method='nelder-mead', args=(m_empirical, ages))
+
+# Extract esimates and print
+params = gmm_estimate.x
+
+param_names = ["rho", "sigma_sq_ups", "sigma_sq_eta", "sigma_sq_alpha"]
+output_text = "GMM Estimates:\n"
+for i,j in enumerate(param_names):
+    output_text +=  j +": " + str(np.round(params[i], 4) ) +  "\n"
+
+print(output_text)
